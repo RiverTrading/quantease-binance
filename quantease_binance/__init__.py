01c05367e4678338ba9bffcdbdd8a37f501965d2
River.Shi <nachuan.shi.quant@gamil.com>
2024-11-07 02:59:11 +0000

    Update version to 0.1.10 in pyproject.toml and refactor import statements in __init__.py

--- quantease_binance/__init__.py
+++ quantease_binance/__init__.py
@@ -1,16 +1,11 @@
-import sys
-
 from .api import fetch_klines, fetch_agg_trades, fetch_data, fetch_book_ticker, fetch_funding_rate, fetch_trades, fetch_metrics, fetch_all_symbols, SymbolType
 
 from importlib import metadata
-import platform
 
-if platform.system() == "Windows":
-    __version__ = "1.0.0"
-else:
-    __version__ = "1.0.0"
+try:
+    __version__ = metadata.version("quantease-binance")
+except metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
 
-
-del metadata, sys, platform
 
 __all__ = ["fetch_klines", "fetch_agg_trades", "fetch_data", "fetch_book_ticker", "fetch_funding_rate", "fetch_trades", "fetch_metrics", "fetch_all_symbols", "SymbolType"]
