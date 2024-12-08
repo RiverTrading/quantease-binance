182e60200952def380d6924b85bd68ae120a0c52
River.Shi <nachuan.shi.quant@gamil.com>
2024-12-08 16:28:51 +0000

    Switch documentation theme from sphinx_rtd_theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -19,5 +19,5 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
