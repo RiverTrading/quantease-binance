a6e2fcdb92b653ee3634b0c0e81c8c28dede337a
River.Shi <nachuan.shi.quant@gamil.com>
2024-12-09 07:58:43 +0000

    Add logo to documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -21,3 +21,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'furo'
 html_static_path = ['_static']
+
+html_logo = 'logo.png'
